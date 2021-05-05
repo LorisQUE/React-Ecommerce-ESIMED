1a725f6eed831b1aed6d2db8e20ff234db96af9c
Loris Quetglas <36083989+LorisQUE@users.noreply.github.com>
2021-05-05 17:11:13 +0200

    début profil

--- src/pages/profil.jsx
+++ src/pages/profil.jsx
@@ -2,6 +2,7 @@ import axios from 'axios';
 import React, { useContext, useEffect, useState } from 'react'
 import { useHistory } from 'react-router';
 import { AuthContext } from '../App';
+import TableCart from '../components/store/tableCart';
 import { BASE_URL } from '../services/data';
 import { removeLocalUser } from '../services/userService';
 
@@ -9,10 +10,10 @@ export default function Profil() {
     const history = useHistory();
     const context = useContext(AuthContext);
     const [commandes, setCommandes] = useState([]);
-
+    const utilisateur = JSON.parse(localStorage.getItem("utilisateur"));
+    
     useEffect(() => {
         const url = BASE_URL + "commandes";
-        const utilisateur = JSON.parse(localStorage.getItem("utilisateur"));
         console.log("UTILISATEUR : ", utilisateur);
         axios.get(url, { params: { userId: utilisateur.id}})
         .then(res => {
@@ -29,9 +30,18 @@ export default function Profil() {
 
     return (
         <main className="container marg-hauteur">
-            <h1>Profil</h1>
+            <h1>Bonjour {utilisateur.prenom + " " + utilisateur.nom} - Mes commandes</h1>
 
-            
+            {commandes.map(x => {
+                return(
+                    <div className="div-commande">
+                        {console.log("x", x)}
+                        <h3>Commande du {new Date(x.date).toLocaleString()}</h3>
+                        <TableCart articles={x.produits}/>
+                    </div>
+                )
+            })}
+
             <button onClick={handleDeconnexion} className="btn btn-secondary">Déconnexion</button>
         </main>
     )
